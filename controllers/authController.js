edd9b675f3e892675fe886d5bbe6d68db4a73c81
jawad <jawadhalbymakary@gmail.com>
2021-12-12 21:33:38 +0200

    added passwordChangedAfterTokenIssued

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,127 +1,131 @@
-const jwt=require("jsonwebtoken")
-const express=require("express")
-const dotenv=require("dotenv")
-const mongoose=require("mongoose")
-const bcrypt=require("bcrypt")
-const User=require("../models/user")
-const validator=require("validator")
-const {promisify}=require("util")
-const signToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:process.env.JWT_EXPIRES_IN
-    })
-
-}
-const createSendToken=(user,statusCode,res)=>{
-    // mongoose create this _id
-    const token=signToken(user._id)
-    // console.log(token)
-    res.status(statusCode).json({
-        status:"success",
-        token,
-        data:{
-            user
-        }
-    })
-
-
-}
-exports.signup= async (req,res)=>{
-    try{
-        const emailCheck=await User.findOne({email:req.body.email})
-        if(emailCheck){
-            return res.status(409).json({
-                message:"email already exists"
-            })
-        }
-        if(!validator.isEmail(req.body.email)){
-            return res.status(400).json({
-                message:"invalid email"
-            })
-        }
-        if(req.body.password !== req.body.passwordConfirm){
-            return res.status(400).json({
-                message:"password does not match"
-            })
-        }
-        const newUser=await User.create({
-            email:req.body.email,
-            password:req.body.password,
-            name:req.body.name,
-            passwordConfirm:req.body.passwordConfirm,
-            passwordChangedAt:req.body.passwordChangedAt,
-        })
-
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const validator = require("validator");
+const { promisify } = require("util");
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+const createSendToken = (user, statusCode, res) => {
+  // mongoose create this _id
+  const token = signToken(user._id);
+  // console.log(token)
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+exports.signup = async (req, res) => {
+  try {
+    const emailCheck = await User.findOne({ email: req.body.email });
+    if (emailCheck) {
+      return res.status(409).json({
+        message: "email already exists",
+      });
     }
-    catch(err){
-        res.status(404).json({
-            status:"fail",
-            message:err
-        })
-       
-    }   
-}
-exports.login=async (req,res)=>{
-    try{
-        const {email,password}=req.body
-        const user=await User.findOne({email})
-        if(!user || !(await user.checkPassword(password,user.password))){
-            return res.status(400).json({
-                message:"email does not exist"
-            })
-        }       
-        createSendToken(user,200,res)
+    if (!validator.isEmail(req.body.email)) {
+      return res.status(400).json({
+        message: "invalid email",
+      });
     }
-    catch(err){
-        res.status(404).json({
-            status:"fail",
-            message:err
-        })
+    if (req.body.password !== req.body.passwordConfirm) {
+      return res.status(400).json({
+        message: "password does not match",
+      });
     }
-}
-exports.protect=async (req,res,next)=>{
-    try{
-        let token;
-        // if there is a token it will be in the header
-        // bearer is found by default
-        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
-            // split the token from the bearer
-            token=req.headers.authorization.split(" ")[1]
-        }
-        if(!token){
-            return res.status(401).json({
-                message:"you are not logged in"
-            })
-        }
-        // verify the token
-        let decoded;
-        try{
-            decoded=await promisify(jwt.verify)(token,process.env.JWT_SECRET)
-        }catch(err){
-            // if the token is expired
-          if(err.name==="TokenExpiredError"){
-            return res.status(401).json({
-                message:"token expired"
-            })
-            // if the token is invalid
-          } else if(err.name==="JsonWebTokenError"){
-            return res.status(401).json({
-                message:"invalid token"
-            })
-        }}
-        // check if the user still exists
-        const currentUser=await User.findById(decoded.id)
-        if(!currentUser){
-            return res.status(401).json({
-                message:"you are not logged in"
-            })
-        }
-        req.user=currentUser
-        next()
-
-    }catch(err){
-        res.status(404).json({
-            status:"fail",
-            message:err
-        })
-    }} 
+    const newUser = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      name: req.body.name,
+      passwordConfirm: req.body.passwordConfirm,
+      passwordChangedAt: req.body.passwordChangedAt,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.checkPassword(password, user.password))) {
+      return res.status(400).json({
+        message: "email does not exist",
+      });
+    }
+    createSendToken(user, 200, res);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.protect = async (req, res, next) => {
+  try {
+    let token;
+    // if there is a token it will be in the header
+    // bearer is found by default
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      // split the token from the bearer
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return res.status(401).json({
+        message: "you are not logged in",
+      });
+    }
+    // verify the token
+    let decoded;
+    try {
+      decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if the token is expired
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({
+          message: "token expired",
+        });
+        // if the token is invalid
+      } else if (err.name === "JsonWebTokenError") {
+        return res.status(401).json({
+          message: "invalid token",
+        });
+      }
+    }
+    // check if the user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return res.status(401).json({
+        message: "you are not logged in",
+      });
+    }
+    // if user changed the password after getting the JWT
+    if (currentUser.passwordChangedAfterTokenIssued(decoded.id)) {
+      return res.status(401).json({
+        message: "your psw has been recently changed. please login again",
+      });
+    }
+    // any new req has a variable currentUser(like creating a session)
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -20,29 +20,54 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
 
   { timestamps: true }
 );
 userSchema.pre("save", async function (next) {
-    try{
-        if(!this.isModified('password')) return next();
-        this.password = await bcrypt.hash(this.password, 12);
-        this.passwordConfirm = undefined;
-        next();
-    } 
-    catch(err){
-        console.log(err);
-        next(err);
-    }})
-userSchema.methods.checkPassword = async function(
-    candidatePassword,
-    userPassword
-  ) {
-    try {
-      return await bcrypt.compare(candidatePassword, userPassword);
-    } catch (err) {
-      throw err;
-    }
+  try {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
+});
+userSchema.methods.checkPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  try {
+    return await bcrypt.compare(candidatePassword, userPassword);
+  } catch (err) {
+    throw err;
+  }
+};
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) {
+    return next();
+  }
+  // for accurate res of passwordChangedAt
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.methods.passwordChangedAfterTokenIssued = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const passwordChangeTime = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < passwordChangeTime;
+
+
+  }
+  return false
+};
+
 module.exports = mongoose.model("User", userSchema);
